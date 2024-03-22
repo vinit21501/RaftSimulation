1d1b180cfc141dd5c832f2cff107e4fbb6eacee1
vinit <vinit21501@iiitd.ac.in>
2024-03-22 12:48:02 +0530

    some concept is correct and state machine is added

--- raft.py
+++ raft.py
@@ -14,36 +14,94 @@ clusterIp = ['10.0.0.1', '10.0.0.2']
 senderPort = '50051'
 recieverPort = '50052'
 nodeTotal = 5
-node = 1
-currentTerm = 0
-votedFor = None
-currentRole = 'follower'
-currentLeader = None
-logDic = {}
+# node = 1
 
-# initialisation
-log = 'NO-OP'
-commitLength = 0
+# currentTerm = 0
+# votedFor = None
 
-currentLeader = None
-votesReceived = {}
-sentLength = 'hi'
-ackedLength = 'h'
+class State():
+    def __init__(self, nodeId):
+        self.nodeId = nodeId
+        self.currentTerm = 0
+        self.votedFor = None
+        self.logs = []
+        self.commitIndex = 0
+        self.lastApplied = 0
+        self.currentlogTerm = 0
+        self.nextIndex = []
+        self.matchIndex = []
+        self.logsDic = {}
+        self.currentRole = 'follower'
+        self.currentLeader = None
+        if os.path.exists(f'logs_node_{self.nodeId}/metadata.txt'):
+            with open(f'logs_node_{self.nodeId}/metadata.txt') as file:
+                for line in file.readlines():
+                    line = line.strip(':').split()
+                    if line[0] == 'nodeId':
+                        self.nodeId = line[1]
+                    elif line[0] == 'currentTerm':
+                        self.currentTerm = line[1]
+                    elif line[0] == 'commitIndex':
+                        self.commitIndex = line[1]
+                    elif line[0] == 'lastApplied':
+                        self.lastApplied = line[1]
+        else:
+            os.mkdir(f'logs_node_{self.nodeId}')
+            open(f'logs_node_{self.nodeId}/metadata.txt', 'w').close()
+        self.readLogs()
+    def export(self):
+        with open(f'logs_node_{self.nodeId}/metadata.txt', 'w') as file:
+            file.write(f'nodeId:{self.nodeId}\n')
+            file.write(f'currentTerm:{self.currentTerm}\n')
+            file.write(f'commitIndex:{self.commitIndex}\n')
+            file.write(f'lastApplied:{self.lastApplied}\n')
+    def readLogs(self):
+        if os.path.exists(f'logs_node_{self.nodeId}/log.txt'):
+            with open(f'logs_node_{self.nodeId}/log.txt') as file:
+                file.readline()
+                for line in file.readlines():
+                    if line.startswith('SET'):
+                        line = line.strip().split()
+                        self.logDic[line[1]] = line[2]
+        else:
+            os.mkdir(f'logs_node_{self.nodeId}')
+            open(f'logs_node_{self.nodeId}/log.txt', 'w').close()
+    def appendEntries(self):
+        with open(f'logs_node_{self.nodeId}/log.txt', 'a') as file:
+            for entry in self.logs:
+                file.write(entry + '\n')
+    def commitEntries(self, leaderCommit):
+        if leaderCommit > self.commitLength:
+            for log in self.logs:
+                if log.startswith('SET'):
+                    log = log.strip().split()
+                    self.logDic[log[1]] = log[2]
+            return True
+        return False
 
-nodes = {}
+# # initialisation
+# logs = ['NO-OP']
+# commitLength = 0
 
-# recovery from crash
+# currentLeader = None
+# votesReceived = {}
+# sentLength = 'hi'
+# ackedLength = 'h'
 
-votesReceived = {}
-sentLength = 'hi'
-ackedLength = 'hi'
+# nodes = {}
 
-# node nodeId suspects leader has failed, or on election timeout
-currentTerm = currentTerm + 1
-currentRole = 'candidate'
-votedFor = 'nodeId'
-votesReceived = {'nodeId'}
-lastTerm = 0
+# # recovery from crash
+
+# votesReceived = {}
+# sentLength = 'hi'
+# ackedLength = 'hi'
+
+# # node nodeId suspects leader has failed, or on election timeout
+# currentTerm = currentTerm + 1
+# currentRole = 'candidate'
+# votedFor = 'nodeId'
+# votesReceived = {'nodeId'}
+# lastTerm = 0
 # if log.length > 0 :
 #     lastTerm = log[log.length - 1].term
 # msg = ('VoteRequest', 'nodeId', currentTerm, log.length, lastTerm)
@@ -60,51 +118,42 @@ def callrpc(rpc, req):
         # dump node is not alive
         return None
 
-def writeFile(fileN, req):
-    with open(f'logs_node_{node}/{fileN}.txt', 'a') as file:
-        file.write(req + '\n')
-
 class RaftServicer(raft_pb2_grpc.RaftServicer):
     def ServeClient(self, request, context):
         pass
-    def voteFor(self, request, context):
-        rep = raft_pb2.SuccessReply(False)
-        if votedFor == None:
-            if request.term > currentTerm:
-                votedFor = request.ip
-                cluster.election.cancel()
-                rep.success = True
-                return rep
-            else:
-                return rep
-        else:
-            return rep
-    def RecieveAck(self, request, context):
-        currentLeader = request.ip
-        currentRole = 'follower'
-        rep = raft_pb2.SuccessReply(True)
-        return rep
-    def HeartBeat(self, request, context):
-        request.leasetime
-        rep = raft_pb2.SuccessReply(True)
-        return rep
-    def CommitEntry(self, request, context):
-        # request.leasetime
-        request.term
-        var = request.var
-        val = request.val
-        logDic[var] = val
-        log = f'SET {var} {val} {currentTerm}'
-        rep = raft_pb2.SuccessReply(True)
-        return rep
-    def AppendEntry(self, request, context):
-        rep = raft_pb2.SuccessReply(True)
-        if not log:
-            writeFile('log', log)
-            log = None
+
+    def AppendEntries(self, request, context):
+        rep = raft_pb2.AppendReply()
+        rep.term = stateMachine.currentTerm
+        if request.term >= stateMachine.currentTerm and request.prevLogTerm > stateMachine.currentlogTerm and request.prevLogIndex >= stateMachine.lastApplied:
+            stateMachine.currentTerm = request.term
+            rep.term = stateMachine.currentTerm
+            # request.leaderId
+            # request.leaderCommit
+            stateMachine.commitEntries(request.leaderCommit)
+            stateMachine.logs = rep.entries
+            stateMachine.appendEntries()
+            rep.success = True
             return rep
         rep.success = False
-        return False
+        return rep
+    def RequestVote(self, request, context):
+        rep = raft_pb2.RequestVoteReply()
+        rep.term = stateMachine.currentTerm
+        if votedFor == None and stateMachine.currentRole == 'follower':
+            if request.term >= stateMachine.currentTerm and stateMachine.lastApplied < request.lastLogIndex and stateMachine.currentlogTerm < request.lastLogTerm:
+                votedFor = request.candidateId
+                rep.voteGranted = True
+                # currentLeader = request.candidateId
+                # currentRole = 'follower'
+                # cancel the election or restart
+                cluster.election.cancel()
+                return rep
+            # handle this parameter for vote granted
+            # request.lastLogIndex
+            # request.lastlogTerm
+        rep.voteGranted = False
+        return rep
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -113,7 +162,7 @@ def serve():
     server.start()
     server.wait_for_termination()
 
-class RequestFollower():
+class NodeWorking():
     def __init__(self, nodeCount, ips):
         self.channelList = list(map(lambda ip : grpc.insecure_channel(ip + ':' + senderPort, options=[('grpc.default_timeout_ms', 1000)]), ips))
         self.stubList = list(map(lambda ch : raft_pb2_grpc.RaftStub(ch), self.channelList))
@@ -187,29 +236,34 @@ class RequestFollower():
                 self.appendEntry(var, val)
             elif opt == 2:
                 var = input("Enter your variable: ")
-                if var in logDic:
-                    print(f'value of {var} : {logDic[var]}')
+                if var in stateMachine.logDic:
+                    print(f'value of {var} : {stateMachine.logDic[var]}')
                 else:
                     print(f'{var} is not intialized')
             else:
                 print('invalid option')
         pass
     def appendEntry(self, var, val):
-        req = raft_pb2.receiveVarRequest()
+        req = raft_pb2.AppendRequest()
         # req.leasetime = 
-        req.term = currentTerm
-        req.var = var
-        req.val = val
+        stateMachine.logs = [f'SET {var} {val} {stateMachine.currentTerm}']
+        stateMachine.appendEntries()
+        req.term = stateMachine.currentTerm
+        req.leaderId = stateMachine.nodeId
+        req.prevLogIndex
+        req.entries = stateMachine.logs
+        req.prevLogTerm
+        req.leaderCommit
         ack = []
         for stub in self.stubList:
-            rep = callrpc(stub.CommitEntry, req)
+            rep = callrpc(stub.AppendEntries, req)
             if rep != None:
                 ack.append(rep)
         ackTotal = len(list(filter(lambda ak : ak.success, ack)))
         if ackTotal >= nodeTotal / 2 + 1:
-            logDic[var] = val
-            log = f'SET {var} {val} {currentTerm}'
-            writeFile('log', log)
+            stateMachine.logDic[var] = val
+            # log = f'SET {var} {val} {stateMachine.currentTerm}'
+            # stateMachine.writeFile('log', log)
             req = raft_pb2.heartbeatRequest()
             for stub in self.stubList:
                 callrpc(stub.AppendEntry, req)
@@ -223,23 +277,15 @@ def runNode():
     cluster.electionTimeout()
 
 if __name__ == "__main__":
-    cluster = RequestFollower(nodeTotal, clusterIp)
-    if os.path.exists(f'logs_node_{node}'):
-        with open(f'logs_node_{node}/log.txt') as file:
-            file.readline()
-            for line in file.readlines():
-                if line.startswith('SET'):
-                    line = line.strip().split()
-                    logDic[line[1]] = line[2]
-    else:
-        os.mkdir(f'logs_node_{node}')
-        writeFile('log', 'NO-OP')
-        serving = threading.Thread(target=serve(), daemon=True)
-        serving.start()
-        run = threading.Thread(target=runNode(), daemon=True)
-        run.start()
-        run.join()
-        serving.join()
+    cluster = NodeWorking(nodeTotal, clusterIp)
+    stateMachine = State(nodeId)
+    
+    serving = threading.Thread(target=serve(), daemon=True)
+    serving.start()
+    run = threading.Thread(target=runNode(), daemon=True)
+    run.start()
+    run.join()
+    serving.join()
 
         # writeFile('dump', 'NO-OP')
         # writeFile('metadata', 'NO-OP')
